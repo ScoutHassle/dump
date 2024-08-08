a53f7791bcc800d27526a60872ff1961057ceace
ScoutHassle <scotthalsall@gmail.com>
2024-08-08 17:16:56 +0100

    Create pause.js

---
+++ pause.js
@@ -0,0 +1,7 @@
+const pauseUI = `
+<div id="pauseUI">
+    <!-- Mimic the controls close button -->
+    <div id="pauseCloseButton" onClick="globalLeveler.unpauseGame();"></div>
+    <div id="pauseTitle"></div>
+</div>
+`
